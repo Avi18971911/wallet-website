0033ae90477991e64af983ae59b9536a5a0f8e75
avinash6 <avinash.sg7@gmail.com>
2024-06-28 21:46:11 +0800

    Add username and password state to login

--- single-page-applications/projects/account-app/login/login.component.ts
+++ single-page-applications/projects/account-app/login/login.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  protected username: string;
+  protected password: string;
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.username = "";
+    this.password = "";
+  }
+  onLogin() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 
+  fetchUserData(userId: string) {
+    const apiUrl = `https://yourapi.com/users/${userId}`;
+    this.http.get(apiUrl).subscribe(
+      response => {
+        console.log('User data:', response);
+        // Handle the response data here
+      },
+      error => {
+        console.error('There was an error!', error);
+      }
+    );
+  }
 }
