b4033e06ea8bc747cbc6d4a3f43e37faccbb2573
avinash6 <avinash.sg7@gmail.com>
2024-07-27 10:15:11 +0800

    Place everything through a router outlet

--- single-page-applications/projects/account-app/src/app/app.component.ts
+++ single-page-applications/projects/account-app/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { HttpClientModule } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,10 @@ import { HttpClientModule } from "@angular/common/http";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'account-app';
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.router.navigate(['/Login']);
+  }
 }
--- single-page-applications/projects/account-app/src/app/app.routes.ts
+++ single-page-applications/projects/account-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "/Login", pathMatch: "full" },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Welcome', component: WelcomeComponent },
+];
