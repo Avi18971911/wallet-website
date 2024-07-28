86f2f1d9c629921fe41890cc71ec8c73f50ea6d2
avinash6 <avinash.sg7@gmail.com>
2024-07-28 22:31:56 +0800

    Add welcome and header

---
+++ single-page-applications/projects/account-app/src/app/components/welcome/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'welcome-header',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
---
+++ single-page-applications/projects/account-app/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+
+}
