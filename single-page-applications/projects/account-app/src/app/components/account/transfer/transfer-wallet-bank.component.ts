8ffc844931ff800232ee333e387e119d1e810481
avinash6 <avinash.sg7@gmail.com>
2024-09-05 23:31:49 +0800

    Add router subscription to handle re-routes = reloads

--- single-page-applications/projects/account-app/src/app/components/account/transfer/transfer-wallet-bank.component.ts
+++ single-page-applications/projects/account-app/src/app/components/account/transfer/transfer-wallet-bank.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProgressBarComponent} from "./progress-bar/progress-bar.component";
-import {ActivatedRoute, Router, RouterOutlet} from "@angular/router";
+import {ActivatedRoute, NavigationEnd, Router, RouterOutlet} from "@angular/router";
 import {AccountService} from "../../../services/account.service";
 import {TransferService} from "../../../services/transfer.service";
+import {filter, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-transfer',
@@ -15,10 +16,26 @@ import {TransferService} from "../../../services/transfer.service";
   styleUrl: './transfer-wallet-bank.component.css',
   providers: [TransferService],
 })
-export class TransferWalletBankComponent implements OnInit {
+export class TransferWalletBankComponent implements OnInit, OnDestroy {
   constructor(private router: Router, private route: ActivatedRoute) {}
+  private routerSubscription: Subscription | undefined;
 
   ngOnInit() {
+    this.navigateToInputDetails()
+
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.navigateToInputDetails()
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription)
+      this.routerSubscription.unsubscribe();
+  }
+
+  private navigateToInputDetails() {
     this.router.navigate(['input-details'], {relativeTo: this.route}).catch((error) => {
       console.error(error);
     });
