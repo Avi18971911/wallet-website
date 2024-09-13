f27121069396167cc5d41c935f48d69bb037d41e
avinash6 <avinash.sg7@gmail.com>
2024-09-13 23:42:54 +0800

    Cleanup

--- single-page-applications/projects/account-app/src/app/components/account/transfer/transfer-wallet-bank.component.ts
+++ single-page-applications/projects/account-app/src/app/components/account/transfer/transfer-wallet-bank.component.ts
@@ -27,7 +27,6 @@ export class TransferWalletBankComponent implements OnInit, OnDestroy {
   private ngUnsubscribe = new Subject<void>();
   protected currentStep: number = 1;
   protected dateTime: string = "";
-  transferSuccess = false;
 
   ngOnInit() {
     this.navigateToInputDetails()
@@ -69,13 +68,7 @@ export class TransferWalletBankComponent implements OnInit, OnDestroy {
 
   private navigateToTransferComplete() {
     this.currentStep = 3;
-    this.router.navigate(
-      ['transfer-complete'],
-      {
-        relativeTo: this.route,
-        state: { isSuccess: this.transferSuccess }
-      }
-    ).catch((error) => {
+    this.router.navigate(['transfer-complete'], {relativeTo: this.route,}).catch((error) => {
       console.error(error);
     });
   }
